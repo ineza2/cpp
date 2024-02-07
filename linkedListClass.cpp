eb649f957b4ed7b022ea7d9b47a919a17c3a0001
ineza2 <ineza2ineza@gmail.com>
2024-02-07 11:53:28 +0200

    added inserting at a certain position

--- linkedListClass.cpp
+++ linkedListClass.cpp
@@ -1,52 +1,50 @@
 #include<iostream>
 using namespace std;
-template <typename T>
 
+template <typename T>
 struct Node{
-	T data;
-	Node *next;
-	Node(){	}
-	Node(T d){
-		data=d;
-		next=NULL;	}
+    T data;
+    Node *next;
+    Node() {}
+    Node(T d){
+        data = d;
+        next = NULL;
+    }
 };
 
 template <typename T>
 void printList(Node<T> *head){
-	int count=0;
-	while(head !=NULL){
-		cout<<head->data<<"->";
-		head=head->next;
-		count++;
-	}
-	cout<<"nbr of elements: "<<count<<endl;
+    int count=0;
+    while(head !=NULL){
+        cout<<head->data<<"->";
+        head=head->next;
+        count++;
+    }
+    cout<<"nbr of elements: "<<count<<endl;
 }
+
 template <typename T>
 Node<T>* addAtHead(Node<T> *head, T value){
-	Node<T> *newNode=new Node<T >(value);
-	newNode->next=head;
-	head=newNode;
-	return head;
+    Node<T> *newNode=new Node<T >(value);
+    newNode->next=head;
+    head=newNode;
+    return head;
 }
-//Node<T>* addAtTail(Node<T> *next, T value){
-//	Node<T> *newNode=new Node<T >(value);
-//	newNode->next= next;
-//	next=newNode;
-//	return next;
-//} 
-
 
 template <typename T>
 Node<T>* addAtTail(Node<T> *head, T value) {
     Node<T> *newNode = new Node<T>(value);
+    if (head == NULL) {
+        head = newNode;
+    } else {
         Node<T> *current = head;
         while (current->next != NULL) {
             current = current->next;
         }
         current->next = newNode;
+    }
     return head;
 }
-
 
 template <typename T>
 Node<T>* userData(){
@@ -67,36 +65,60 @@ Node<T>* userData(){
     }
     return head;
 }
+
+template <typename T>
+Node<T>* insertAtPos(Node<T> *head, int i, T data){
+    if(i<0){
+        return head;
+    }
+    if(i==0){
+        Node<T>* n = new Node<T>(data);
+        n->next = head;
+        head = n;
+        return head;
+    }
+    Node<T> *temp = head;
+    int count = 1;
+    while(count<i && temp!=NULL){
+        temp = temp->next;
+        count++;
+    }
+    if(temp){
+        Node<T> *n = new Node<T>(data);
+        n->next = temp->next;
+        temp->next = n;
+    }
+    return head;
+}
+
 int main(){
-	Node<int> *n1=new Node<int>(10);
-//	cout<<n1->data<<endl;
-	Node<int> *n2= new Node<int>(20);
-//	cout<<n2->data<<endl;
-	Node<int> *n3= new Node<int>(30);
-//	cout<<n3->data<<endl;
-n1->next=n2;
-n2->next=n3;
-Node<int> *head=n1;
-printList(head);
-cout<<"after adding element at the head"<<endl;
-head=addAtHead(head,8);
-printList(head);
+    Node<int> *n1=new Node<int>(10);
+    Node<int> *n2= new Node<int>(20);
+    Node<int> *n3= new Node<int>(30);
+    n1->next=n2;
+    n2->next=n3;
+    Node<int> *head=n1;
 
-cout<<"after adding element at the tail"<<endl;
-head=addAtTail(head, 22);
-printList(head);
+    printList(head);
+    cout<<"after adding element at the head"<<endl;
+    head=addAtHead(head,8);
+    printList(head);
 
-//Node* head=userData();
-//print(head);
-//return 0;
+    cout<<"after adding element at the tail"<<endl;
+    head=addAtTail(head, 22);
+    printList(head);
 
-Node<int> *userHead = userData<int>();
+    cout<<"after inserting element at position 4"<<endl;
+    head=insertAtPos(head,4,250);
+    printList(head);
+
+    Node<int> *userHead = userData<int>();
     cout << "User input list:" << endl;
     printList(userHead);
     
-delete n1;
+    delete n1;
     delete n2;
     delete n3;
-	return 0;
+    return 0;
 }
 
