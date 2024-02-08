65a25dbf96a67cbc8dab37164989eee99e9704b9
ineza2 <ineza2ineza@gmail.com>
2024-02-08 09:11:02 +0200

    reversing the list

--- revLinkedListOperations.cpp
+++ revLinkedListOperations.cpp
@@ -1,31 +1,29 @@
 #include<iostream>
+using namespace std;
 struct Node{
 	int data;
 	struct Node *next;
 };
 
-//Node traverseNodes(Node *head){
-	struct Node *temp=head;
-	while(temp !=NULL){
-		println("%d-->", temp->data);
-		temp=temp->next;
-	}
-	
-//} 
+void traverseNodes(Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        cout<<temp->data<< " ,";
+        temp = temp->next;
+    }
+    cout<<"\n"<<endl;
+}
+ 
 
-//Node insertBeginning(int val){
-	
-	struct Node *newNode;
-	newNode=malloc(sizeof(struct Node));
-	newNode->data=val;
-	newNode->next=head;
-	head=newNode;
+Node* insertBeginning(Node* head, int val){
+    Node* newNode = new Node;
+    newNode->data = val;
+    newNode->next = head;
+    return newNode;
+}
 
-//}
-
-//Node insertEnd(int val){
-	struct Node *newNode;
-	newNode= malloc(sizeof(struct Node));
+Node insertEnd(Node *head, int val){
+	Node *newNode=new Node;
 	newNode->data=val;
 	newNode->next=NULL;
 	
@@ -35,9 +33,59 @@ struct Node{
 	}
 	temp->next=newNode;
 
-//}
+}
+
+Node insertAnyPosition(Node *head,int i, int val){
+	Node *newNode=new Node;
+	newNode->data=val;
+	
+	struct Node *temp=head;
+	for(int j=0; j<i;j++){
+		if(temp->next!=NULL){
+			temp=temp->next;
+		}
+	}
+	newNode->next=temp->next;
+	temp->next=newNode;
+	
+}
+
+Node* reverse(Node *head){
+	Node* current = head;
+    Node *prev = NULL, *next = NULL;
+    while (current != NULL) {
+            next = current->next;
+            current->next = prev;
+            
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head; 
+}
 
 
 int main(){
+	Node* n1 = new Node;
+    n1->data = 3;
+    Node* n2 = new Node;
+    n2->data = 7;
+    Node* n3 = new Node;
+    n3->data = 45;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = NULL;
+    
+    Node* head = n1;
+    
+    traverseNodes(head);
+//    head=insertBeginning(head,98);
+//    traverseNodes(head);
+//    insertEnd(head,75);
+//    traverseNodes(head);
+//    insertAnyPosition(head,1,655);
+//    traverseNodes(head);
+    head=reverse(head);
+    traverseNodes(head);
 	
 }
