94997848628abe3108c4ba02ef45a33b49eac41b
ineza2 <ineza2ineza@gmail.com>
2024-02-14 08:43:08 +0200

    insert at head

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -5,34 +5,54 @@ struct node {
     int data;
     struct node *next;
     struct node *prev;
+};
+
+// Function to insert node at the beginning
+node* insertAtHead(node *head, int val){
+    node *newNode = new node(); // Changed Node to node
+    newNode->data = val;
+    newNode->prev = NULL; // Changed null to nullptr
+    newNode->next = head;
+    head = newNode;
+    return head;
 }
 
-/* Initialize nodes */
-struct node *head;
-struct node *one = NULL;
-struct node *two = NULL;
-struct node *three = NULL;
+// Function to print the linked list
+void printList(node *head){ // Changed Node to node
+    while(head != NULL){ // Changed NULL to nullptr
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "NULL" << endl; // Changed \n to endl
+}
 
-/* Allocate memory */
-one = malloc(sizeof(struct node));
-two = malloc(sizeof(struct node));
-three = malloc(sizeof(struct node));
+int main(){
+    struct node *head = NULL; // Changed null to nullptr
+    
+    struct node *one = new node();
+    struct node *two = new node();
+    struct node *three = new node();
 
-/* Assign data values */
-one->data = 1;
-two->data = 2;
-three->data = 3;
+    // Assign data values
+    one->data = 11;
+    two->data = 8;
+    three->data = 45;
+    
+    one->next = two;
+    one->prev = NULL;
 
-/* Connect nodes */
-one->next = two;
-one->prev = NULL;
+    two->next = three;
+    two->prev = one;
 
-two->next = three;
-two->prev = one;
+    three->next = NULL;
+    three->prev = two;
 
-three->next = NULL;
-three->prev = two;
-
-/* Save address of first node in head */
-head = one;
+    /* Save address of first node in head */
+    head = one;
+    
+    head = insertAtHead(head, 98);
+    printList(head);
+    
+    return 0; // Added return statement
+}
 
